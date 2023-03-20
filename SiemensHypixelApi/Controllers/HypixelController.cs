6b59b92aa6fcde5ec5685742704983168acacaa0
daFeichtl <71697693+daFeichtl@users.noreply.github.com>
2023-03-20 09:08:05 +0100

    removed parent folder

---
+++ SiemensHypixelApi/Controllers/HypixelController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using ApiTest.Dtos;
+using ApiTest.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTest.Controllers;
+
+[ApiController]
+[Route("hypixel")]
+public class HypixelController
+{
+    private readonly HypixelService _service;
+
+    public HypixelController(HypixelService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("collections")]
+    public async Task<ActionResult<List<CollectionDto>?>> GetCollections()
+    {
+        return await _service.GetCollections();
+    }
+
+    [HttpGet("items")]
+    public async Task<ActionResult<List<ItemDto>?>> GetAllItems()
+    {
+        return await _service.GetAllItems();
+    }
+
+    [HttpGet("accessories")]
+    public ActionResult<List<ItemDto>?> GetAllAccessories()
+    {
+        return _service.GetAllAccessories();
+    }
+}
